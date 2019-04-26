f91ae2288f9f4624b91d0b00946b48b41070ae2f
1 <root@i12.informatica.escoladeltreball.org>
2019-04-26 10:29:05 +0200

    dsasd

--- ip-09-dmz.sh
+++ ip-09-dmz.sh
@@ -1,8 +1,41 @@
+#! /bin/bashdhc
+#edt ASIX m11 2018-2019
+#Raul Baena Nocea
+#Exemples iptables
+#----------------------------
+echo 1 > /proc/sys/net/ipv4/ip_forward
+#Regles flush
+iptables -F #Flush the selected chain
+iptables -X # Delete  the  optional user-defined chain specified. Cadenas personalitzadas
+iptables -Z #Eleimina la cadena
+iptables -t nat -F #-t This  option specifies the packet matching table which the command should operate on.
 
+#RPolitiques per defecte
+iptables -P INPUT  ACCEPT #Per defecte DROP o ACCEPT
+iptables -P OUTPUT ACCEPT 
+iptables -P FORWARD ACCEPT
+#iptables -t nat -P PREROUTING ACCEPT
+#iptables -t nat -P POSTROUTING ACCEPT #totes aquestes comandes es un no a tot
+# obrir el localhost
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+# obrir la nostra ip
+iptables -A INPUT -s 192.168.2.42 -j ACCEPT
+iptables -A OUTPUT -d 192.168.2.42 -j ACCEPT
+
+ #Fer NAT per les xarxes internes:
+iptables -t nat -A POSTROUTING -s 172.23.0.0/24 -o enp6s0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.24.0.0/24 -o enp6s0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.25.0.0/24 -o enp6s0 -j MASQUERADE
 #Configurar un host/router amb dues xarxes privades locals xarxaA i xarxaB i una tercera xarxa DMZ amb servidors: 
 #nethost, ldap, kerberos, samba. Implementar-ho amb containers i xarxes docker.
-
 #Exercicis a implementar:
 #(1) des d'un host exterior accedir al servei ldap de la DMZ. Ports 389, 636.
+iptables -t nat -A PREROUTING -i enp5s0 -p tcp --dport 389 -j DNAT --to 172.25.0.3:389
+iptables -t nat -A PREROUTING -i enp5s0 -p tcp --dport 636 -j DNAT --to 172.25.0.6:636
 #(2) des d'un host exterior, engegar un container kclient i obtenir un tiket kerberos del servidor de la DMZ. Ports: 88, 543, 749.
+iptables -t nat -A PREROUTING -i enp5s0 -p tcp --dport 88 -j DNAT --to 172.25.0.4:88
+iptables -t nat -A PREROUTING -i enp5s0 -p tcp --dport 543 -j DNAT --to 172.25.0.4:543
+iptables -t nat -A PREROUTING -i enp5s0 -p tcp --dport 749 -j DNAT --to 172.25.0.4:749
 #(3) des d'un host exterior muntar un recurs samba del servidor de la DMZ.
+iptables -t nat -A PREROUTING -i enp5s0 -p tcp --dport 2049 -j DNAT --to 172.25.0.4:2049
--- ip-10-denied.sh
+++ ip-10-denied.sh
@@ -35,8 +35,8 @@ iptables -A OUTPUT -p udp -d 208.67.222.222 --dport 53 -m state --state NEW,ESTA
 iptables -A INPUT  -p udp -s 208.67.222.222 --sport 53 -m state --state ESTABLISHED     -j ACCEPT
 
 #dhclient(68)
-iptables -A INPUT -p udp -m state --state ESTABLISHED --dport 68 -j ACCEPT
-iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED --sport 68 -j ACCEPT
+iptables -A INPUT -p udp -m state --state ESTABLISHED --sport 68 -j ACCEPT
+iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED --dport 68 -j ACCEPT
 
 # icmp
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
@@ -49,54 +49,54 @@ iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --sport 443 -m tcp -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 #ssh (22)
-iptables -A INPUT -i enp5s0 -p tcp --dport 22 -m state --state ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o enp5s0 -p tcp --sport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i enp5s0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o enp5s0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 
 #rpc 111, 507
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 111 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 111 -j ACCEPT
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 507 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 507 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 111 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 111 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 507 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 507 -j ACCEPT
 
 #chronyd 123, 371
-iptables -A OUTPUT -p udp --dport 123 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT  -p udp --sport 123 -m state --state ESTABLISHED     -j ACCEPT
-iptables -A OUTPUT -p udp --dport 371 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT  -p udp --sport 371 -m state --state ESTABLISHED     -j ACCEPT
+iptables -A OUTPUT -p udp --sport 123 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT  -p udp --dport 123 -m state --state ESTABLISHED     -j ACCEPT
+iptables -A OUTPUT -p udp --sport 371 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT  -p udp --dport 371 -m state --state ESTABLISHED     -j ACCEPT
 
 #cups 631
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 631 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 631 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 631 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 631 -j ACCEPT
 
 #xinetd 3411
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 3411 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED  --sport 3411 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 3411 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED  --dport 3411 -j ACCEPT
 
 #postgresql 5432
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 5432 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED  --sport 5432 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 5432 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED  --dport 5432 -j ACCEPT
 
 #x11forwarding 6010, 6011
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 6010 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 6010 -j ACCEPT
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 6011 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 6011 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 6010 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 6010 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 6011 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 6011 -j ACCEPT
 
 #avahi 368
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 368 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 368 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 368 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 368 -j ACCEPT
 
 #alpes 462
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 462 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 462 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 462 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 462 -j ACCEPT
 
 #tcpnethaspsrv 475
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 475 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 475 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 475 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 475 -j ACCEPT
 
 #rxe 761
-iptables -A INPUT -p tcp -m state --state ESTABLISHED --dport 761 -j ACCEPT
-iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --sport 761 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED --sport 761 -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED --dport 761 -j ACCEPT
 
 #ldap
 iptables -A OUTPUT -p tcp --dport 389 -j ACCEPT
@@ -111,4 +111,7 @@ iptables -A OUTPUT -p tcp --dport 464 -j ACCEPT
 iptables -A INPUT -p tcp --sport 464 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 749 -j ACCEPT
 iptables -A INPUT -p tcp --sport 749 -j ACCEPT
-#
+
+#nfs
+iptables -A OUTPUT -p tcp --dport 2049 -j ACCEPT
+iptables -A INPUT -p tcp --sport 2049 -j ACCEPT
